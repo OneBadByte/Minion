dba1ae0fc8f0959201f577154c4e8ffb29323447
blackdartq <brody.prestwich18@gmail.com>
2018-12-25 06:28:28 -0700

    added comments and option that will kill the server

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,75 @@
 import java.net.*;
 import java.io.*;
-import java.util.Arrays;
+import java.util.ArrayList;
 
+class MinionConnection {
+    private Socket socket;
 
+    // used to connect to other minions
+    MinionConnection(String ipAddress, int port){
+        while(true){
+            try {
+                this.socket = new Socket(ipAddress, port);
+                break;
+            } catch (IOException e) {
+            }
+        }
+//        System.out.println("Connected to: " + ipAddress + ":" + port);
+    }
+
+    // sends a command to other minions
+    public void send(String message){
+        try {
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+            output.println(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // receives input from another minion
+    public void receive(){
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            String message = "";
+            message = reader.readLine();
+            message = message.replace("|", "\n");
+//            System.out.println(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
+
+// Server is the brains of a minion
 class Server{
+    private boolean keepAlive = true;
     private int port;
     private ServerSocket serverSocket;
     private Socket socket;
+
+    // holds all of the minions that this minion is connected to
+    ArrayList<MinionConnection> minionConnections = new ArrayList<>();
 
     Server(int port){
         this.port = port;
         this.accept();
     }
 
+    // waits and accepts a connection
     public void accept(){
         try {
             this.serverSocket = new ServerSocket(this.port);
             this.socket = this.serverSocket.accept();
-            System.out.println("Connected");
+//            System.out.println("Connected");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    // gets commands from a minion or gru
     public String read(){
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
@@ -33,72 +80,112 @@ class Server{
         }
     }
 
-    public void close(){
+    public void respond(String response){
+        try {
+            PrintWriter writer = new PrintWriter(this.socket.getOutputStream(), true);
+            writer.print(response + "\n");
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // closes a socket
+    public void close() {
         try {
             this.serverSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-}
 
-
-class Command{
-
-    public void processCommand(String command){
-        String[] commandArray = command.split(" ");
-        String commandString = "";
-        for(int x=1; x < commandArray.length; x++){
-           commandString = commandString + " " + commandArray[x];
-        }
-
-        if(commandArray[0].equals("linux")){
-            linuxCommand(commandString);
-        }
+    public void killServer(){
+        this.keepAlive = false;
     }
 
-    public void linuxCommand(String command){
+    public boolean isServerAlive(){
+        return this.keepAlive;
+    }
 
+    // processes the input received and executes appropriate function
+    public void processCommand(String command) {
+        // splits command by whitespace and stores it in an array
+        String[] commandArray = command.split(" ");
+
+        // TODO fix this section
+        String commandString = "";
+        for (int x = 1; x < commandArray.length; x++) {
+            commandString = commandString + " " + commandArray[x];
+        }
+
+        switch (commandArray[0]) {
+            case "linux":
+                linuxCommand(commandString);
+                break;
+            case "connect":
+                this.respond("in progress");
+                break;
+            case "kill":
+                this.respond("in progress");
+                break;
+            case "die":
+                this.killServer();
+                break;
+            default:
+                this.respond(command);
+                break;
+        }
+        //___________________
+    }
+
+    // runs a linux command
+    public void linuxCommand(String command){
         try {
+            // process runs the command and waits for it to finish
             Process process = Runtime.getRuntime().exec(command);
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            while (reader.readLine() != null){
-                System.out.println(reader.readLine());
-            }
             process.waitFor();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
+            // stores everything in one line separated by | instead of new lines
+            String message = "";
+            //ghetto hack so that the reader wont keep running through lines
+            String storage = "";
+            while ((storage = reader.readLine()) != null){
+                message = message + storage + "|";
+            }
+            // sends the response message
+            this.respond(message);
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-
     }
-
 }
-
 
 public class Main {
 
     public static void main(String[] args) {
-        Server server = new Server(5000);
-        Command command = new Command();
+        // starts the server on the port provided
+        Server server = new Server(Integer.parseInt(args[0]));
         while(true){
             String input = "";
 
-            while(true){
+            // checks if the server is alive and will end when changed
+            while(server.isServerAlive()){
+                // gets user input sent over tcp
                 input = server.read();
                 if(input == null){
                     break;
                 }
-                System.out.println(input);
-                if(input == "quit"){
-                    server.close();
-                    break;
-                }
-                command.processCommand(input);
+                server.processCommand(input);
             }
+            // closes the socket
             server.close();
-            server.accept();
+            if(server.isServerAlive()) {
+                // will start a new connection
+                server.accept();
+            }else{
+                break;
+            }
         }
+        System.out.println("GoodBye Cruel World!");
     }
 }
